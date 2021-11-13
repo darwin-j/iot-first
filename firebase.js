cce21c1d0c2c5ba9b288b50c5e5e59f01cde4f49
Your Name <darwinmuthu450@gmail.com>
2021-11-13 08:16:34 +0530

    simple led on/off done

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db };
--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    apiKey: "AIzaSyBc_8mwl7QNuaceXTMWaFfTIYAo87Kldk8",
+    authDomain: "iot-first-project-9126b.firebaseapp.com",
+    databaseURL:
+      "https://iot-first-project-9126b-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "iot-first-project-9126b",
+    storageBucket: "iot-first-project-9126b.appspot.com",
+    messagingSenderId: "226847140432",
+    appId: "1:226847140432:web:f505b8dfdd5d674f5b7f55",
+  },
+};
